4b32d7e51610c0ee24579c98cbae1b1b9b3e3fc3
Rj1ng <rj1ngring@gmail.com>
2018-11-28 12:06:38 +0800

    edited run-analysis.sh script

--- run-analysis.sh
+++ run-analysis.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 echo "IOS App analysis";
+echo "sh run-ayalysis.sh binary ir_filename opt_ir_filename llvm_slicer_path report_filename rules_path";
 
-binary_file = $1;
-#substr = ${binary_file%%.*}
-ir_file = ${binary_file}".ir"
-opt_ir_file = ${binary_file}"-opt.ir"
+$binary_file = $1;
+#substr = ${binary_file%%.*};
+$ir_file = ${binary_file}".ir";
+$opt_ir_file = ${binary_file}"-opt.ir";
+$llvm_slicer = $2;
+$report_file = $3;
+$rule_file = $4;
 
-
+./build/bin/llvm-dec ${binary_file} -O1 -bc -o ${ir_file}
+./build/bin/opt -instcombine -sroa -simplifycfg -constprop ${ir_file} -o ${opt_ir_file}
+${llvm_slicer} -binary ${binary_file} -o /dev/null -r ${report_file} -rules ${rule_file}
 
